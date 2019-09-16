771f1408ca047d44a3a85d2d4b812c0bce601a86
tama <tama@ttk1.net>
2019-09-17 08:42:55 +0900

    諸々修正

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 window.onload = () => {
-  // WebGL 2.0コンテキストを取得する
   const canvas = document.getElementById('canvas') as HTMLCanvasElement;
   const gl = canvas.getContext('webgl2') as WebGL2RenderingContext;
 
@@ -7,7 +6,7 @@ window.onload = () => {
   gl.getExtension('OES_texture_float_linear');
   gl.getExtension('EXT_color_buffer_float');
 
-  // 定数
+  // 定数 <- 何？
   const N = 512;
 
   // 背景を白にする
@@ -32,83 +31,118 @@ window.onload = () => {
     }
     m.push(1.0e+2);
   }
-  const iindex = new Float32Array(index);
-  const pp = new Float32Array(p);
-  const vv = new Float32Array(v);
-  const aa = new Float32Array(a);
-  const mm = new Float32Array(m);
+  const I = new Float32Array(index);
+  const P = new Float32Array(p);
+  const V = new Float32Array(v);
+  const A = new Float32Array(a);
+  const M = new Float32Array(m);
 
-  // テクスチャを生成する
-  const ppTex: WebGLTexture[] = [];
-  const vvTex: WebGLTexture[] = [];
-  const aaTex: WebGLTexture[] = [];
-  const mmTex: WebGLTexture[] = [];
-  ppTex[0] = transfer_data(gl, pp, 4, gl.RGBA32F, gl.RGBA, gl.FLOAT);
-  vvTex[0] = transfer_data(gl, vv, 4, gl.RGBA32F, gl.RGBA, gl.FLOAT);
-  aaTex[0] = transfer_data(gl, aa, 4, gl.RGBA32F, gl.RGBA, gl.FLOAT);
-  mmTex[0] = transfer_data(gl, mm, 1, gl.R32F, gl.RED, gl.FLOAT);
-  ppTex[1] = transfer_data(gl, pp, 4, gl.RGBA32F, gl.RGBA, gl.FLOAT);
-  vvTex[1] = transfer_data(gl, vv, 4, gl.RGBA32F, gl.RGBA, gl.FLOAT);
-  aaTex[1] = transfer_data(gl, aa, 4, gl.RGBA32F, gl.RGBA, gl.FLOAT);
+  // テクスチャの生成
+  function transferData(list: Float32Array,
+                        dimension: number, iformat: number,
+                        format: number, type: number) {
+    const texture = gl.createTexture();
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    gl.texImage2D(gl.TEXTURE_2D, 0, iformat, list.length / dimension, 1, 0,
+                  format, type, list);
+    gl.bindTexture(gl.TEXTURE_2D, null);
+    return texture;
+  }
 
-  // シェーダをコンパイル
-  const vs = compile_shader(gl, gl.VERTEX_SHADER,  require('./glsl/vert.glsl').default);
-  const fs = compile_shader(gl, gl.FRAGMENT_SHADER,  require('./glsl/frag.glsl').default);
+  const pTex: WebGLTexture[] = [];
+  const vTex: WebGLTexture[] = [];
+  const aTex: WebGLTexture[] = [];
+  const mTex: WebGLTexture[] = [];
+  pTex[0] = transferData(P, 4, gl.RGBA32F, gl.RGBA, gl.FLOAT);
+  vTex[0] = transferData(V, 4, gl.RGBA32F, gl.RGBA, gl.FLOAT);
+  aTex[0] = transferData(A, 4, gl.RGBA32F, gl.RGBA, gl.FLOAT);
+  mTex[0] = transferData(M, 1, gl.R32F, gl.RED, gl.FLOAT);
+  pTex[1] = transferData(P, 4, gl.RGBA32F, gl.RGBA, gl.FLOAT);
+  vTex[1] = transferData(V, 4, gl.RGBA32F, gl.RGBA, gl.FLOAT);
+  aTex[1] = transferData(A, 4, gl.RGBA32F, gl.RGBA, gl.FLOAT);
 
-  // シェーダをリンクする
-  const program = link_shader(gl, vs, fs);
+  // シェーダプログラムの設定
+  function getShader(type: number, source: string) {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+      throw new Error(gl.getShaderInfoLog(shader));
+    }
+    return shader;
+  }
 
+  const program = gl.createProgram();
+  const vertexShader = getShader(gl.VERTEX_SHADER, require('./glsl/vert.glsl').default);
+  gl.attachShader(program, vertexShader);
+  const fragmentShader = getShader(gl.FRAGMENT_SHADER, require('./glsl/frag.glsl').default);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  gl.useProgram(program);
+
+  // これ何？
   let n = 0;
+  let step = 0;
+  const maxStep = 1000;
 
-  swapping();
+  // 実行開始!
+  swap();
 
-  function swapping() {
-    // GPGPUシェーダを使用
-    gl.useProgram(program);
+  function swap() {
+    // ステップ数確認
+    if (step++ >= maxStep) {
+      showResult();
+      return;
+    }
 
     // in変数をVBOと関連付ける
+    // indexBufferはIBOと被るからやめたい
     const indexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, indexBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, iindex, gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, I, gl.STATIC_DRAW);
     const loc = gl.getAttribLocation(program, 'index');
     gl.enableVertexAttribArray(loc);
     gl.vertexAttribPointer(loc, 1, gl.FLOAT, false, 0, 0);
     gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
     // フレームバッファをバインドする
-    const f = gl.createFramebuffer();
-    gl.bindFramebuffer(gl.FRAMEBUFFER, f);
+    const fb = gl.createFramebuffer();
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
 
     // テクスチャをレンダーターゲットに指定
-    gl.bindFramebuffer(gl.FRAMEBUFFER, f);
+    gl.bindFramebuffer(gl.FRAMEBUFFER, fb);
     gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0,
-      gl.TEXTURE_2D, ppTex[(n % 2) ? 0 : 1], 0);
+      gl.TEXTURE_2D, pTex[(n % 2) ? 0 : 1], 0);
     gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT1,
-      gl.TEXTURE_2D, vvTex[(n % 2) ? 0 : 1], 0);
+      gl.TEXTURE_2D, vTex[(n % 2) ? 0 : 1], 0);
     gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT2,
-      gl.TEXTURE_2D, aaTex[(n % 2) ? 0 : 1], 0);
+      gl.TEXTURE_2D, aTex[(n % 2) ? 0 : 1], 0);
     gl.drawBuffers([gl.COLOR_ATTACHMENT0, gl.COLOR_ATTACHMENT1,
     gl.COLOR_ATTACHMENT2]);
 
     // uniform変数とテクスチャを関連付ける
     gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, ppTex[(n % 2) ? 1 : 0]);
+    gl.bindTexture(gl.TEXTURE_2D, pTex[(n % 2) ? 1 : 0]);
     gl.uniform1i(gl.getUniformLocation(program, 'p'), 0);
 
     gl.activeTexture(gl.TEXTURE1);
-    gl.bindTexture(gl.TEXTURE_2D, vvTex[(n % 2) ? 1 : 0]);
+    gl.bindTexture(gl.TEXTURE_2D, vTex[(n % 2) ? 1 : 0]);
     gl.uniform1i(gl.getUniformLocation(program, 'v'), 1);
 
     gl.activeTexture(gl.TEXTURE2);
-    gl.bindTexture(gl.TEXTURE_2D, aaTex[(n % 2) ? 1 : 0]);
+    gl.bindTexture(gl.TEXTURE_2D, aTex[(n % 2) ? 1 : 0]);
     gl.uniform1i(gl.getUniformLocation(program, 'a'), 2);
 
     gl.activeTexture(gl.TEXTURE3);
-    gl.bindTexture(gl.TEXTURE_2D, mmTex[0]);
+    gl.bindTexture(gl.TEXTURE_2D, mTex[0]);
     gl.uniform1i(gl.getUniformLocation(program, 'm'), 3);
 
     gl.activeTexture(gl.TEXTURE4);
-    gl.bindTexture(gl.TEXTURE_2D, ppTex[(n % 2) ? 1 : 0]);
+    gl.bindTexture(gl.TEXTURE_2D, pTex[(n % 2) ? 1 : 0]);
     gl.uniform1i(gl.getUniformLocation(program, 'global_p'), 4);
 
     // 描画命令
@@ -120,42 +154,14 @@ window.onload = () => {
     } else {
       n = 0;
     }
-    requestAnimationFrame(swapping);
+    requestAnimationFrame(swap);
+  }
+
+  // 結果表示用
+  function showResult() {
+    gl.readBuffer(gl.COLOR_ATTACHMENT0);
+    const readingBuffer = new Float32Array(N * 4);
+    gl.readPixels(0, 0, N, 1, gl.RGBA, gl.FLOAT, readingBuffer);
+    console.log(readingBuffer);
   }
 };
-
-function transfer_data(gl: WebGL2RenderingContext, list: Float32Array,
-                       dimension: number, iformat: number,
-                       format: number, type: number) {
-  const tex = gl.createTexture();
-  gl.bindTexture(gl.TEXTURE_2D, tex);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-  gl.texImage2D(gl.TEXTURE_2D, 0, iformat, list.length / dimension, 1, 0,
-    format, type, list);
-  gl.bindTexture(gl.TEXTURE_2D, null);
-
-  return tex;
-}
-
-function compile_shader(gl: WebGL2RenderingContext,
-                        type: number, source: string) {
-  const s = gl.createShader(type);
-  gl.shaderSource(s, source);
-  gl.compileShader(s);
-  if (!gl.getShaderParameter(s, gl.COMPILE_STATUS)) {
-    throw new Error(gl.getShaderInfoLog(s));
-  }
-  return s;
-}
-
-function link_shader(gl: WebGL2RenderingContext,
-                     vs: WebGLShader, fs: WebGLShader) {
-  const p = gl.createProgram();
-  gl.attachShader(p, vs);
-  gl.attachShader(p, fs);
-  gl.linkProgram(p);
-  return p;
-}
